40b3af9d00f9bf56914c0e961c574e8f1bf89c56
pavangite2002 <pavangite2002@gmail.com>
2024-03-19 14:59:53 +0530

    api gateway implemented successfully

--- api_gateway/src/main/java/com/pavangite/ApiGatewayApplication.java
+++ api_gateway/src/main/java/com/pavangite/ApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.pavangite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
